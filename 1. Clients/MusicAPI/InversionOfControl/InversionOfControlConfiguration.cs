eaa1952b19430b7fa1cad0acd3200879ddcee0dc
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-01-18 17:26:01 -0600

    Initial setup of Auth accessor to request Access token for Spotify API

---
+++ 1. Clients/MusicAPI/InversionOfControl/InversionOfControlConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using MusicAPI.Accessors;
+using MusicAPI.Accessors.Interfaces;
+
+namespace MusicAPI.InversionOfControl
+{
+    public static class InversionOfControlConfiguration
+    {
+        public static void ConfigureAccessorDependencies(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IAuthorizationAccessor, AuthorizationAccessor>();
+        }
+    }
+}
---
+++ MusicAPI.Accessors/AuthorizationAccessor.cs
@@ -0,0 +1,21 @@
+﻿using MusicAPI.Accessors.Interfaces;
+
+namespace MusicAPI.Accessors
+{
+    public class AuthorizationAccessor : IAuthorizationAccessor
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthorizationAccessor(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<string> RequestAccessToken()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+
+            return Task.Run(() => string.Empty);
+        }
+    }
+}
