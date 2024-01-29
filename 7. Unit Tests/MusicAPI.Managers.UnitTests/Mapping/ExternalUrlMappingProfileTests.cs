365a39ea8392534ee10772b8c5568241e32adc1f
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-01-29 10:07:19 -0600

    Primary Constructor update to ConfigurationManager for consistency, added ExternalUrlMappingProfile unit tests

---
+++ 7. Unit Tests/MusicAPI.Managers.UnitTests/Mapping/ExternalUrlMappingProfileTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MusicAPI.Managers.Mapping;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MusicAPI.Managers.UnitTests.Mapping
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class ExternalUrlMappingProfileTests
+    {
+        private IMapper Mapper { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mapperConfiguration = new MapperConfiguration(configuration =>
+            {
+                configuration.AddProfile(typeof(ExternalUrlMappingProfile));
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Test]
+        public void ExternalUrlMappingProfile_AssertConfigurationIsValid()
+        {
+            // Act & Assert
+            Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void ExternalUrl_DataTransferObject_To_ViewModel_ShouldMap()
+        {
+            // Arrange
+            const string expectedUrl = "SpotifyTest";
+
+            var externalUrlDTO = new Accessors.DataTransferObjects.ExternalUrl
+            {
+                Spotify = expectedUrl
+            };
+
+            // Act
+            var externalUrlViewModel = Mapper.Map<ViewModels.ExternalUrl>(externalUrlDTO);
+
+            // Assert
+            Assert.That(externalUrlViewModel.Spotify, Is.EqualTo(expectedUrl));
+        }
+    }
+}
