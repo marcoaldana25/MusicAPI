c16dc3069f886838daa65be0990e9b5d1913d164
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-01-29 10:25:26 -0600

    Add followers mapping profile unit tests

---
+++ 7. Unit Tests/MusicAPI.Managers.UnitTests/Mapping/FollowersMappingProfileTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MusicAPI.Managers.Mapping;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MusicAPI.Managers.UnitTests.Mapping
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class FollowersMappingProfileTests
+    {
+        private IMapper Mapper { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mapperConfiguration = new MapperConfiguration(configuration =>
+            {
+                configuration.AddProfile(typeof(FollowersMappingProfile));
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Test]
+        public void FollowersMappingProfile_AssertConfigurationIsValid()
+        {
+            // Act & Assert
+            Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Followers_DataTransferObject_To_ViewModel_ShouldMap()
+        {
+            // Arrange
+            const string expectedHref = "href";
+            const int expectedTotal = 100;
+
+            var followersDTO = new Accessors.DataTransferObjects.Followers
+            {
+                Href = expectedHref,
+                Total = expectedTotal
+            };
+
+            // Act
+            var followersViewModel = Mapper.Map<ViewModels.Followers>(followersDTO);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(followersViewModel.Href, Is.EqualTo(expectedHref));
+                Assert.That(followersViewModel.Total, Is.EqualTo(expectedTotal));
+            });
+        }
+    }
+}
