15cac3607a9bc802f0d7304dd2b50ec3c7d0a90d
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-02-01 10:35:26 -0600

    Initial create of GetSearchAsync endpoint. Updated program.cs to generate Swagger documentation specific to this API

--- 1. Clients/MusicAPI/Program.cs
+++ 1. Clients/MusicAPI/Program.cs
@@ -1,13 +1,32 @@
 using MusicAPI.InversionOfControl;
+using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    // Serialize enums as strings for incoming requests
+    var enumConverter = new JsonStringEnumConverter();
+    options.JsonSerializerOptions.Converters.Add(enumConverter);
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(configuration =>
+{
+    configuration.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Music API",
+        Version = "v1",
+        Description = "A .NET 8.0 Web API that integrates with Spotify's Web API to retrieve data"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    configuration.IncludeXmlComments(xmlPath);
+});
 
 // Allow IHttpClientFactory to be injected to necessary classes.
 builder.Services.AddHttpClient();
