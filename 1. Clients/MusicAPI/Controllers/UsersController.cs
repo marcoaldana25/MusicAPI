a65ddf4d78551d91f2fdb64f329cee99afeb5a3f
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-02-02 14:03:13 -0600

    GetArtistAsync accessor unit test coverage. Also split controllers up to be specific to the data its retrieving

---
+++ 1. Clients/MusicAPI/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Managers.Interfaces;
+using MusicAPI.Managers.ViewModels.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicAPI.Controllers
+{
+    /// <summary>
+    /// Controller for Client integration targeting Spotify's Web API, specifially around Users information.
+    /// </summary>
+    /// <param name="spotifyManager"></param>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(ISpotifyManager spotifyManager) : ControllerBase
+    {
+        /// <summary>
+        ///     Get detailed profile information about the current user (including the current user's username)
+        /// </summary>
+        /// <returns>
+        ///     Detailed profile information about the current authorized user.
+        /// </returns>
+        [HttpGet]
+        [Route("/GetAccountDetails")]
+        [Produces(typeof(OkObjectResult))]
+        public async Task<IActionResult> GetAccountDetails()
+        {
+            var spotifyAccount = await spotifyManager
+                .GetSpotifyAccountAsync();
+
+            return Ok(spotifyAccount);
+        }
+    }
+}
