e2257bc8afe2b4dbdd2b2e6ee969cd2f382feec9
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-02-20 15:21:58 -0600

    Unit test coverage for Playlist and Playlists

---
+++ 7. Unit Tests/MusicAPI.Managers.UnitTests/Mapping/PlaylistMappingProfileTests.cs
@@ -0,0 +1,81 @@
+﻿namespace MusicAPI.Managers.Tests.Mapping
+{
+#pragma warning disable CS8602
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class PlaylistMappingProfileTests
+    {
+        private IMapper Mapper { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mapperConfiguration = new MapperConfiguration(configuration =>
+            {
+                configuration.AddProfile(typeof(PlaylistMappingProfile));
+                configuration.AddProfile(typeof(ExternalUrlMappingProfile));
+                configuration.AddProfile(typeof(ImageMappingProfile));
+                configuration.AddProfile(typeof(OwnerMappingProfile));
+                configuration.AddProfile(typeof(FollowersMappingProfile));
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Test]
+        public void PlaylistMappingProfile_AssertConfigurationIsValid()
+        {
+            // Act & Assert
+            Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Playlist_DataTransferObject_To_ViewModel_ShouldMap()
+        {
+            // Arrange
+            const string expectedDescription = "description";
+            const string expectedHref = "href";
+            const string expectedName = "name";
+            const string expectedSnapshotId = "Snapshot id";
+            const string expectedType = "type";
+            const string expectedUri = "uri";
+
+            var playlistDataTransferObject = new Accessors.DataTransferObjects.Playlist
+            {
+                Collaborative = true,
+                Description = expectedDescription,
+                ExternalUrls = new Accessors.DataTransferObjects.ExternalUrl(),
+                Href = expectedHref,
+                Images =
+                [
+                    new Accessors.DataTransferObjects.Image()
+                ],
+                Name = expectedName,
+                Owner = new Accessors.DataTransferObjects.Owner(),
+                Public = true,
+                SnapshotId = expectedSnapshotId,
+                Type = expectedType,
+                Uri = expectedUri
+            };
+
+            // Act
+            var playlistViewModel = Mapper.Map<ViewModels.Playlist>(playlistDataTransferObject);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(playlistViewModel.Collaborative, Is.True);
+                Assert.That(playlistViewModel.Description, Is.EqualTo(expectedDescription));
+                Assert.That(playlistViewModel.ExternalUrls, Is.Not.Null);
+                Assert.That(playlistViewModel.Href, Is.EqualTo(expectedHref));
+                Assert.That(playlistViewModel.Images, Is.Not.Null);
+                Assert.That(playlistViewModel.Name, Is.EqualTo(expectedName));
+                Assert.That(playlistViewModel.Owner, Is.Not.Null);
+                Assert.That(playlistViewModel.Public, Is.True);
+                Assert.That(playlistViewModel.SnapshotId, Is.EqualTo(expectedSnapshotId));
+                Assert.That(playlistViewModel.Type, Is.EqualTo(expectedType));
+                Assert.That(playlistViewModel.Uri, Is.EqualTo(expectedUri));
+            });
+        }
+    }
+}
---
+++ 7. Unit Tests/MusicAPI.Managers.UnitTests/Mapping/PlaylistsMappingProfileTests.cs
@@ -0,0 +1,71 @@
+﻿namespace MusicAPI.Managers.Tests.Mapping
+{
+#pragma warning disable CS8602
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class PlaylistsMappingProfileTests
+    {
+        private IMapper Mapper { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mapperConfiguration = new MapperConfiguration(configuration =>
+            {
+                configuration.AddProfile(typeof(PlaylistsMappingProfile));
+                configuration.AddProfile(typeof(PlaylistMappingProfile));
+                configuration.AddProfile(typeof(ExternalUrlMappingProfile));
+                configuration.AddProfile(typeof(ImageMappingProfile));
+                configuration.AddProfile(typeof(OwnerMappingProfile));
+                configuration.AddProfile(typeof(FollowersMappingProfile));
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Test]
+        public void PlaylistsMappingProfile_AssertConfigurationIsValid()
+        {
+            // Act & Assert
+            Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Playlists_DataTransferObject_To_ViewModel_ShouldMap()
+        {
+            // Arrange
+            const string expectedPlaylistsHref = "Expected Playlists Href";
+            const string expectedNext = "Expected Next";
+            const string expectedPrevious = "Expected Previous";
+
+            var playlistsDataTransferObject = new Accessors.DataTransferObjects.Playlists
+            {
+                Href = expectedPlaylistsHref,
+                Limit = 1,
+                Next = expectedNext,
+                Offset = 2,
+                Previous = expectedPrevious,
+                Total = 3,
+                Items =
+                [
+                    new Accessors.DataTransferObjects.Playlist()
+                ]
+            };
+
+            // Act
+            var playlistsViewModel = Mapper.Map<ViewModels.Playlists>(playlistsDataTransferObject);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(playlistsViewModel.Href, Is.EqualTo(expectedPlaylistsHref));
+                Assert.That(playlistsViewModel.Limit, Is.EqualTo(1));
+                Assert.That(playlistsViewModel.Next, Is.EqualTo(expectedNext));
+                Assert.That(playlistsViewModel.Offset, Is.EqualTo(2));
+                Assert.That(playlistsViewModel.Previous, Is.EqualTo(expectedPrevious));
+                Assert.That(playlistsViewModel.Total, Is.EqualTo(3));
+                Assert.That(playlistsViewModel.Items, Is.Not.Null);
+            });
+        }
+    }
+}
